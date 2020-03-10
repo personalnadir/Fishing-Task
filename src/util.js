8583b7c27598e3e6f50318d3f338a3955ff22227
Tom Piercy <tap32@medschl.cam.ac.uk>
2020-03-10 11:38:22 +0000

    Move populate array func to util file

--- src/util.js
+++ src/util.js
@@ -10,3 +10,34 @@ export default function shuffle(array) {
 
 	return array;
 }
+
+export function populateArray(arr, genValue, start, end) {
+	// Steps 3-5.
+	var len = arr.length >>> 0;
+
+	// Steps 6-7.
+	var relativeStart = start >> 0;
+
+	// Step 8.
+	var k = relativeStart < 0 ?
+	Math.max(len + relativeStart, 0) :
+	Math.min(relativeStart, len);
+
+	// Steps 9-10.
+	var relativeEnd = end === undefined ?
+	len : end >> 0;
+
+	// Step 11.
+	var finalValue = relativeEnd < 0 ?
+	Math.max(len + relativeEnd, 0) :
+	Math.min(relativeEnd, len);
+
+	// Step 12.
+	while (k < finalValue) {
+		arr[k] = genValue();
+		k++;
+	}
+
+	// Step 13.
+	return arr;
+}
