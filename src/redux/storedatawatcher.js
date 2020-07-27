cfd3761541cc1f4af83b3f76af9dd9ed434bf6cc
Tom Piercy <tap32@medschl.cam.ac.uk>
2020-07-27 09:22:28 +0100

    Handle start and end of phase correctly

--- src/redux/storedatawatcher.js
+++ src/redux/storedatawatcher.js
@@ -3,33 +3,35 @@ import store from './store';
 import getTrial, {
 	getPhase,
 	getCurrentTrialIndex,
-	getTrialRepeated,
 	collectTrialDataDuringPhase
 } from './selectors';
-import {setPhase, setTrialBlockStart, setTrial, submitRowOfData} from './dataactions';
 import {PHASE1} from './globalconstants';
+import {setPhase, setTrial, submitRowOfData} from './dataactions';
 
 let watchTrialIndex = watch(() => {
 	const state = store.getState();
 	if (!collectTrialDataDuringPhase(state)) {
 		return null;
 	}
-	const phase = getPhase(state);
-
 	return getCurrentTrialIndex(state);
 });
 
 store.subscribe(watchTrialIndex((newVal, oldVal) => {
 	const state = store.getState();
-	if (newVal === null || oldVal == null) {
+	if (newVal === null) {
+		store.dispatch(submitRowOfData());
 		return;
 	}
 	const trial = getTrial(state);
 	if (!trial) {
 		return;
 	}
-	store.dispatch(setTrial(getCurrentTrialIndex(state), trial, Date.now()));
+	if (oldVal === null) {
+		store.dispatch(setTrial(getCurrentTrialIndex(state), trial, Date.now()));
+		return;
+	}
 	store.dispatch(submitRowOfData());
+	store.dispatch(setTrial(getCurrentTrialIndex(state), trial, Date.now()));
 }));
 
 let watchPhase = watch(() => getPhase(store.getState()));
